ceae2afcd403f14759c859fa22f1446792f3427f
Maxime Charland <charland.maxime@gmail.com>
2022-10-08 13:40:05 -0400

    Create tests for PhaseService

--- tm-server/src/services/phase/phase.service.spec.ts
+++ tm-server/src/services/phase/phase.service.spec.ts
@@ -1,15 +1,35 @@
+import { Firestore, DocumentReference, CollectionReference } from '@google-cloud/firestore';
 import { Test, TestingModule } from '@nestjs/testing';
+import { IPhase } from '@tm/types/models/datamodels';
 import { PhaseService } from './phase.service';
+import { ROOT_DOC } from '//config/constants';
+import { initFirestore, closeFirestore } from '//test/test-base';
 
 describe('PhaseService', () => {
+  let db: Firestore;
+  let root: DocumentReference;
   let service: PhaseService;
+  let collection: CollectionReference<IPhase>;
+
+  beforeAll(async () => {
+    ({ db, root } = initFirestore());
+    collection = root.collection('phase') as CollectionReference<IPhase>;
+  });
+
+  afterAll(async () => {
+    await closeFirestore({ db, root });
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PhaseService],
+      providers: [PhaseService, { provide: ROOT_DOC, useValue: root }],
     }).compile();
 
     service = module.get<PhaseService>(PhaseService);
+  });
+
+  afterEach(async () => {
+    await db.recursiveDelete(collection);
   });
 
   it('should be defined', () => {
